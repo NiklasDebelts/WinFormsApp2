d23f147a141fbbbc1ef5cadf35261041aded187d
Niklas Debelts <debeltsnik@gmail.com>
2026-01-09 08:50:51 +0100

    hab was geändert

--- WinFormsApp2/Form1.Designer.cs
+++ WinFormsApp2/Form1.Designer.cs
@@ -28,12 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
